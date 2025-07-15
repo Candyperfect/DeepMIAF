9b4bafed55c082df6a1f81bef611ebd0f4b818f8
Candyperfect <563628482@qq.com>
2025-07-15 15:01:51 +0800

    Update preprocessing.sh

--- scripts/preprocessing.sh
+++ scripts/preprocessing.sh
@@ -2,8 +2,8 @@
 
 python data_preprocessing.py
 python seq_embedding.py -m ./esm2_t33_650M_UR50D/esm2_t33_650M_UR50D.pt
-python struct2graph.py
 python text_embedding.py
+python struct2graph.py
 python struct_embedding.py -ont mf -m ./structmodel
 python struct_embedding.py -ont bp -m ./structmodel
 python struct_embedding.py -ont cc -m ./structmodel
